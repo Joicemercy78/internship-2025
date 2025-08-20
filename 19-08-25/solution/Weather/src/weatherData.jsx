0840510f4c65559ddd02ad4e489f10edf723367c
Joicemercy78 <joicemercy7867@gmail.com>
2025-08-20 07:24:18 +0530

    Created Weather UI

---
+++ 19-08-25/solution/Weather/src/weatherData.jsx
@@ -0,0 +1,8 @@
+const weatherData = [
+    { city: "Chennai, Tamilnadu", temperature: 30, condition: "Sunny" },
+    { city: "Coimbatore, Tamilnadu", temperature: 28, condition: "Cloudy" },
+    { city: "Madurai, Tamilnadu", temperature: 32, condition: "Rainy" },
+  ];
+
+  export default weatherData;
+  
